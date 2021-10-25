3c8dbc34443ade145a9c7ddc88f5c885f3eac815
Julia-if <juliaif18@gmail.com>
2021-10-25 05:44:34 +0300

    index

---
+++ src/js/menudev.js
@@ -0,0 +1,7 @@
+import template from "../templates/menu.hbs";
+import data from "../menu.json";
+
+const menuList = document.querySelector('js-menu');
+const item = template(data);
+console.log(item);
+menuList.insertAdjacentHTML("afterbegin", items);
